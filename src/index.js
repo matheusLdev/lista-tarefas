93ff9c542fb1ff05e84396966279cdc3aca996eb
Matheus Gonçalves <goncalves.mld@hotmail.com>
2023-02-13 17:11:59 -0300

    Incluindo bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Index from './pages/index';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import {Index} from './pages/index';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Index />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
