a59ea215b0395d512f2ba851c1a4f4ddbb20e2a9
Matheus Gonçalves <goncalves.mld@hotmail.com>
2023-02-13 17:13:52 -0300

    Incluindo a estilização da página.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+import './style/global.css';
+import './style/style.css';
 import {Index} from './pages/index';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -9,3 +11,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Index />
   </React.StrictMode>,
 )
+
